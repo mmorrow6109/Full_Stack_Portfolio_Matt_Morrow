8e7675ee15ea820bab921163cd14a364abb9145c
mmorrow6109 <msmorrow6109@gmail.com>
2023-11-09 22:12:50 -0800

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Full_Stack_Portfolio_Matt_Morrow
+# Challenge-2-Portfolio
+
+## Description
+
+For this week's challenge, we were to put to practice everything we've learned about CSS and HTML thus far.  The goal was to build a portfolio website from scratch, so that we can add our work to it as the term goes on.  This was an excellent challenge that allowed us to hone in on our creative side and really make the page about who we are.  
+
+I prefer more succinct and concise websites that aren't filled with massive images, so in order to meet the parameters of the challenge, i wanted to demonstrate the functionality of the page, while keeping it relatively simple.  I chose to implement JS elements to expand the targeted boxes, rather than have the link take you way down to another section of the page.  
+
+Much more demanding than last week, this challenge was WILDLY helpful in building competence and confidence with my newfound coding skillset.
+
+## Credits
+
+Matt Morrow
+
+## License
+
+MIT License
+
+## Features
+
+- All links functional ("Work" in the Nav is functional, it just doesn't have a place to take the user as it's in the center of the page)
+- Nav links
+- Project link
+- About Me section
+- JavaScript functions to expand "About Me" and "Contact" elements
+- Images displayed correctly
+- Alt tags attached to images
+- Semantically correct
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-// document.getElementById('aboutme-link').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     var isClickInside = document.getElementById('aboutme-aside').contains(event.target);
-//     var isAsideExpanded = document.getElementById('aboutme-aside').classList.contains('expand');
-
-//     if (!isClickInside && isAsideExpanded) {
-//         document.getElementById('aboutme-aside').classList.remove('expand');
-//     }
-//     else {document.getElementById('aboutme-aside').classList.add('expand');}
-// });
-
 document.getElementById('aboutme-link').addEventListener('click', function(event) {
     event.preventDefault();
     var isClickInside = document.getElementById('aboutme-aside').contains(event.target);
@@ -18,4 +7,15 @@ document.getElementById('aboutme-link').addEventListener('click', function(event
         document.getElementById('aboutme-aside').classList.remove('expand');
     }
     else {document.getElementById('aboutme-aside').classList.add('expand');}
+});
+
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    var isClickInside = document.getElementById('contact').contains(event.target);
+    var isAsideExpanded = document.getElementById('contact').classList.contains('expand2');
+
+    if (!isClickInside && isAsideExpanded) {
+        document.getElementById('contact').classList.remove('expand2');
+    }
+    else {document.getElementById('contact').classList.add('expand2');}
 });
